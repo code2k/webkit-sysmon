80428d3afc5f66f6c99353ead408017c7baa555a
Andreas Pataki <andreas.pataki@code2k.net>
2018-05-06 17:16:42 +0200

    Initial commit

---
+++ webkit-sysmon/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  webkit-sysmon
+//
+//  Created by Andreas Pataki on 06.05.18.
+//  Copyright © 2018 CODE2K:LABS. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ webkit-sysmon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  webkit-sysmon
+//
+//  Created by Andreas Pataki on 06.05.18.
+//  Copyright © 2018 CODE2K:LABS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
